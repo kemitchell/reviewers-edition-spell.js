7c0ed6a24eca1de3c6817a6eaca9682d90dc86c1
Kyle E. Mitchell <kyle@kemitchell.com>
2017-07-14 17:27:00 -0700

    Add bin script

---
+++ bin.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+var spell = require('./')
+require('pump')(
+  process.stdin,
+  require('split2')(),
+  require('through2').obj(function (chunk, encoding, done) {
+    var line = chunk.toString(encoding)
+    var spelled = spell(line)
+    done(null, (spelled ? spelled : line) + '\n')
+  }),
+  process.stdout
+)
